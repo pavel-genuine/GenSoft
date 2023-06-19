6de704f047c18f4a85574379b650f55cc8413be1
pavel.genuine <pavel.playstore@gmail.com>
2023-06-19 18:42:33 +0600

    team

--- components/common/Card.js
+++ components/common/Card.js
@@ -5,15 +5,19 @@ import { HiOutlineArrowRight } from "react-icons/hi"
 export const Card = ({ data, caption, show, path }) => {
   return (
     <>
-      <div className='card'>
-        <div className='card-img'>
-          <img src={data.cover} alt={data.title} />
+      <div className='card '>
+        <div className='card-img flex justify-center items-center'>
+          <img className="" src={data.cover} alt={data.title} />
         </div>
         <div className='card-details'>
           {/* <Link href={`${path}/${data.id}`} className='title-link'> */}
-            <a href={data.url} target="_blank" rel="noopener noreferrer">
+          <a href={data.url} target="_blank" rel="noopener noreferrer">
             <TitleSm title={data.title} />
-            </a>
+            
+          
+          </a>
+          <p>
+              {data?.post}</p>
           {/* </Link> */}
           {/* {caption && (
             <Link href={`${path}/${data.id}`}>
@@ -33,6 +37,7 @@ export const Card = ({ data, caption, show, path }) => {
           )}
         </div>
       </div>
+      
     </>
   )
 }
